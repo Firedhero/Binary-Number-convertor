314afda3e7704522b7cc5dde047c8cdf0314a1b0
Firedhero <54608663+Firedhero@users.noreply.github.com>
2019-09-03 13:24:54 -0400

    Update Run.java

--- Run.java
+++ Run.java
@@ -2,7 +2,7 @@
 public class Run {
 	public static void main(String[]args)
 	{
-		System.out.println("Enter the binary numbers");
+		System.out.println("Enter the deseried Input");
 		
 		Logic.convertor();
 		
