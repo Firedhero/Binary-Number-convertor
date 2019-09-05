bb85eb781fb1f9e01e32bf797f5f65c725a817fc
Firedhero <54608663+Firedhero@users.noreply.github.com>
2019-09-04 20:33:02 -0400

    Add files via upload

---
+++ Logic_2.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+
+public class Logic_2 {
+
+	private static String Uncon = null;
+	static Scanner scan=new Scanner(System.in);
+	
+	private static void Input() {
+		Uncon=scan.next();
+		convertor(Uncon);
+	}
+	
+	private static void convertor(String uncon2) {
+		
+		
+		
+		
+	}
+	
+	private static void BinaryNum(int Con) {
+		
+		switch(Con) {
+		case 0:
+		
+			break;
+		case 1:
+		
+		}
+	}
+	private static void BinaryLet(String Con) {
+		
+		switch(Con) {
+		case "a":
+		
+		}
+		
+	}
+	
+	
+	
+	
+	
+	public static void main(String[] args) {
+		Input();
+		
+		
+
+	}
+
+}
